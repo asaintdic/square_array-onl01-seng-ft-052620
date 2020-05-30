8b2ffa75b6f56161df965c05645ad9601215d287
Alain Saint-Dic <alains214@gmail.com>
2020-05-30 02:39:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 
 def square_array(array)
-  numbers =[1, 2, 3]
-  numbers.each do |number|
+  numbers = [1, 2, 3]
+  new_array
+  numbers.each |number|
     new_numbers = ["#{number}**2"]
   end  
 square_array("numbers")
