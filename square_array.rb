6ee9c2ff1d1808d004c0895483ed256e860ada46
Alain Saint-Dic <alains214@gmail.com>
2020-05-30 04:08:56 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,11 +2,8 @@ def square_array(array)
   arrays = []
   array.each{|numbers| arrays << numbers ** 2
   arrays
-
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
 end
+
+
 
 
